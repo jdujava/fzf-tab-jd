b3c7a5514535b8fba1d0f406aad8f29083a9ae63
Jonas Dujava <jonas.dujava@gmail.com>
2023-12-01 00:04:57 +0100

    and also for paru (AUR helper)

--- fzf-tab-jd.plugin.zsh
+++ fzf-tab-jd.plugin.zsh
@@ -16,6 +16,8 @@ zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl
 
 zstyle ':fzf-tab:complete:pacman:*' fzf-flags "--preview-window=right:70%:nohidden:nowrap"
 zstyle ':fzf-tab:complete:pacman:*' fzf-preview 'pacman -Qii $word'
+zstyle ':fzf-tab:complete:paru:*' fzf-flags "--preview-window=right:70%:nohidden:nowrap"
+zstyle ':fzf-tab:complete:paru:*' fzf-preview 'pacman -Qii $word'
 
 zstyle ':fzf-tab:complete:cd:*' fzf-flags "--preview-window=right:nohidden:nowrap"
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'tree -L 1 -C --dirsfirst $realpath' # remember to use single quote here!!!
